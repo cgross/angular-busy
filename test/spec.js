e8a36ee9f3c26fbd213f8dd053b4818a54d0f033
benbracha <ben.bracha@ravellosystems.com>
2015-11-05 17:42:36 +0200

    Return accidently removed UT from angular-busy

--- test/spec.js
+++ test/spec.js
@@ -32,6 +32,58 @@ describe('cgBusy', function() {
 
 		expect(this.element.children().css('display')).toBe('block');//ensure its visible (promise is ongoing)
 
+		this.testPromise.resolve();
+		scope.$apply();
+
+		expect(this.element.children().css('display')).toBe('none'); //ensure its now invisible as the promise is resolved
+	});
+
+	it('should show the overlay during multiple promises', function() {
+
+		this.element = compile('<div cg-busy="[my_promise,my_promise2]"></div>')(scope);
+		angular.element('body').append(this.element);
+
+		this.testPromise = q.defer();
+		scope.my_promise = this.testPromise.promise;
+
+		this.testPromise2 = q.defer();
+		scope.my_promise2 = this.testPromise2.promise;
+
+		//httpBackend.flush();
+
+		scope.$apply();
+
+		expect(this.element.children().length).toBe(2); //ensure the elements are added
+
+		expect(this.element.children().css('display')).toBe('block');//ensure its visible (promise is ongoing)
+
+		this.testPromise.resolve();
+		scope.$apply();
+
+		expect(this.element.children().css('display')).toBe('block'); //ensure its still visible (promise is ongoing)
+
+		this.testPromise2.resolve();
+		scope.$apply();
+		expect(this.element.children().css('display')).toBe('none'); //ensure its now invisible as the promise is resolved
+	});
+
+
+	it('should show the overlay when changing an unresolved promise with other unresolved promise', function() {
+
+		this.element = compile('<div cg-busy="my_promise"></div>')(scope);
+		angular.element('body').append(this.element);
+
+		this.testPromise = q.defer();
+		scope.my_promise = this.testPromise.promise;
+
+		//httpBackend.flush();
+
+		scope.$apply();
+
+		expect(this.element.children().length).toBe(2); //ensure the elements are added
+
+		expect(this.element.children().css('display')).toBe('block');//ensure its visible (promise is ongoing)
+
 		// Change promise
 		this.testPromise2 = q.defer();
 		scope.my_promise = this.testPromise2.promise;
