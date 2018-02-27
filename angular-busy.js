4e08f4acc16bb54180b199aac812f336d68b1348
Rumit Parakhiya <rumitparakhiya@users.noreply.github.com>
2018-02-27 17:18:04 +0530

    Removing embedded template

--- angular-busy.js
+++ angular-busy.js
@@ -252,35 +252,3 @@ angular.module('cgBusy').directive('cgBusy',['$compile','$templateCache','cgBusy
     }
 ]);
 
-
-angular.module('cgBusy').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('angular-busy.html',
-    "<div class=\"cg-busy-default-wrapper\">\n" +
-    "\n" +
-    "   <div class=\"cg-busy-default-sign\">\n" +
-    "\n" +
-    "      <div class=\"cg-busy-default-spinner\">\n" +
-    "         <div class=\"bar1\"></div>\n" +
-    "         <div class=\"bar2\"></div>\n" +
-    "         <div class=\"bar3\"></div>\n" +
-    "         <div class=\"bar4\"></div>\n" +
-    "         <div class=\"bar5\"></div>\n" +
-    "         <div class=\"bar6\"></div>\n" +
-    "         <div class=\"bar7\"></div>\n" +
-    "         <div class=\"bar8\"></div>\n" +
-    "         <div class=\"bar9\"></div>\n" +
-    "         <div class=\"bar10\"></div>\n" +
-    "         <div class=\"bar11\"></div>\n" +
-    "         <div class=\"bar12\"></div>\n" +
-    "      </div>\n" +
-    "\n" +
-    "      <div class=\"cg-busy-default-text\">{{$message}}</div>\n" +
-    "\n" +
-    "   </div>\n" +
-    "\n" +
-    "</div>"
-  );
-
-}]);
